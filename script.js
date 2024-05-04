33ea15a5b772450e1323b0d90ff20e7c2df4ac1a
blurfrost <ksyc07@gmail.com>
2024-05-04 15:43:07 +0800

    Added functions to restart the game after it ends and to prevent the game from extending beyond a fixed number of rounds

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
- // initialize human and computer's scores as zero
+// initialize human and computer's scores, as well as round number as zero
 let humanScore = 0;
 let computerScore = 0;
+let roundNumber = 0;
 
 // relate to scoreboard tag in html
 const scoreboard = document.querySelector("#scoreboard");
@@ -9,36 +10,48 @@ const displayScore = document.createElement("div");
 // set initial content to initial score (0 for each player) and append to the scoreboard div
 displayScore.textContent = "Current Score - You: " + humanScore + " Computer: " + computerScore;
 scoreboard.appendChild(displayScore);
-// create element to display winner
+// create element to display winners of each round and the entire game respectively
 const roundWinner = document.createElement("div");
 scoreboard.appendChild(roundWinner);
 const gameWinner = document.createElement("div");
 gameWinner.textContent = "";
 scoreboard.appendChild(gameWinner);
 
-let roundNumber = 0;
 // event listener to check for clicks on the button area
 let buttons = document.querySelector("#buttons");
-    buttons.addEventListener("click", (event) => {
-        let target = event.target;
-        switch(target.id) {
-            case "rock":
-                playRound("rock", getComputerChoice());
-                checkWinner();
-                break;
-            case "paper":
-                playRound("paper", getComputerChoice());
-                checkWinner();
-                break;
-            case "scissors":
-                playRound("scissors", getComputerChoice());
-                checkWinner();
-                break;
-        }
-    });
+buttons.addEventListener("click", makeChoice)
+    
+// function for event listener for rock, paper, scissors buttons
+function makeChoice(event) {
+    let target = event.target;
+    switch(target.id) {
+        case "rock":
+            playRound("rock", getComputerChoice());
+            break;
+        case "paper":
+            playRound("paper", getComputerChoice());
+            break;
+        case "scissors":
+            playRound("scissors", getComputerChoice());
+            break;
+    }
+    checkWinner();
+}
 
+// checks for winner each round, if 5 rounds have passed, then evaluate scores
 function checkWinner () {
     if (roundNumber === 5) {
+        // provides a button to reload the page to restart the game (appears only when all 5 rounds are finished)
+        const reloadButton = document.createElement("button");
+        reloadButton.textContent = "Click me or reload the page to restart the game!";
+        scoreboard.appendChild(reloadButton);
+        // event listener for reload button
+        reloadButton.addEventListener("click", () => {
+            location.reload();
+        });
+        // removes event listener for rock, paper, scissors options to prevent the game from going beyond round 5
+        buttons.removeEventListener("click", makeChoice)
+        // display when you win
         if (humanScore > computerScore) {
             gameWinner.textContent = "You win!";
         }
@@ -51,10 +64,9 @@ function checkWinner () {
             gameWinner.textContent = "It's a draw!";
         }
     }
-    
 }
 
-// function that returns either rock, paper, or scissors
+// function that returns either rock, paper, or scissors as the computer's choice
 function getComputerChoice() {
     // determine random value, multiply math.random by 3 to get a number between 0 (inclusive) and 3 (exclusive)
     value = Math.random() * 3;
@@ -78,6 +90,7 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    // increases round number as soon as you click one of the options
     roundNumber += 1;
     // case where the round ends in a draw (both players choose the same)
     if (humanChoice === computerChoice) {
@@ -105,5 +118,6 @@ function playRound(humanChoice, computerChoice) {
     else {
         return resultText = "Error";
     }
+    // update score at the end of the round
     displayScore.textContent = "(Round " + roundNumber + ") Current Score - You: " + humanScore + " Computer: " + computerScore;
 }
